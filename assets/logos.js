a9ed646a57c772c5f02f4229678934913b8b2b48
Saizuo <71127774+Saizuo@users.noreply.github.com>
2024-01-14 20:46:52 +0530

    new images

--- assets/logos.js
+++ assets/logos.js
@@ -1,11 +1,65 @@
-import image1 from "./images/logos/logo1.jpg";
-import image2 from "./images/logos/logo2.jpg";
-import image3 from "./images/logos/logo3.jpg";
-import image4 from "./images/logos/logo4.jpg";
-import image5 from "./images/logos/logo5.jpg";
-import image6 from "./images/logos/logo6.jpg";
-import image7 from "./images/logos/logo7.jpg";
-import image8 from "./images/logos/logo8.jpg";
-import image9 from "./images/logos/logo9.gif";
+import image1 from "./images/logos/1.png";
+import image2 from "./images/logos/2.png";
+import image3 from "./images/logos/3.png";
+import image4 from "./images/logos/4.png";
+import image5 from "./images/logos/5.png";
+import image6 from "./images/logos/6.png";
+import image7 from "./images/logos/7.png";
+import image8 from "./images/logos/8.png";
+import image9 from "./images/logos/9.png";
+import image10 from "./images/logos/10.png";
+import image11 from "./images/logos/11.gif";
+import image12 from "./images/logos/12.gif";
+import image13 from "./images/logos/13.gif";  
+import image14 from "./images/logos/14.gif";
+import image15 from "./images/logos/15.gif";
+import image16 from "./images/logos/16.gif";
+import image17 from "./images/logos/17.gif";
+import image18 from "./images/logos/18.gif";
+import image19 from "./images/logos/19.gif";
+import image20 from "./images/logos/20.gif";
+import image21 from "./images/logos/21.gif";
+import image22 from "./images/logos/22.gif";
+import image23 from "./images/logos/23.gif";
+import image24 from "./images/logos/24.png";
+import image25 from "./images/logos/25.png";
+import image26 from "./images/logos/26.png";
+import image27 from "./images/logos/27.png";
+import image28 from "./images/logos/28.png";
+import image29 from "./images/logos/29.png";
+import image30 from "./images/logos/30.png";
+import image31 from "./images/logos/31.png";
+import image32 from "./images/logos/32.png";
+import image33 from "./images/logos/33.png";
+import image34 from "./images/logos/34.png";
+import image35 from "./images/logos/35.png";
+import image36 from "./images/logos/36.png";
+import image37 from "./images/logos/37.png";
+import image38 from "./images/logos/38.png";
+import image39 from "./images/logos/39.png";
+import image40 from "./images/logos/40.png";
+import image41 from "./images/logos/41.png";
+import image42 from "./images/logos/42.png";
+import image43 from "./images/logos/43.png";
+import image44 from "./images/logos/44.png";
+import image45 from "./images/logos/45.png";
+import image46 from "./images/logos/46.png";
+import image47 from "./images/logos/47.jpg";
+import image48 from "./images/logos/48.jpg";
+import image49 from "./images/logos/49.jpg";
+import image50 from "./images/logos/50.jpg";
+import image51 from "./images/logos/51.jpg";
+import image52 from "./images/logos/52.jpg";
+import image53 from "./images/logos/53.jpg";
+import image54 from "./images/logos/54.jpg";
+import image55 from "./images/logos/55.gif";
 
-export default [image1, image2, image3, image4, image5, image6, image7, image8, image9];
+export default [
+    image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,
+    image11, image12, image13, image14, image15, image16, image17, image18, image19, image20,
+    image21, image22, image23, image24, image25, image26, image27, image28, image29, image30,
+    image31, image32, image33, image34, image35, image36, image37, image38, image39, image40,
+    image41, image42, image43, image44, image45, image46, image47, image48, image49, image50,
+    image51, image52, image53, image54, image55
+  ];
+  
