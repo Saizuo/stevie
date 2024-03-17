1f69ef85b851c8b15c4a755de652dbd1e0603eae
GenerallyStevie <39565821+GenerallyStevie@users.noreply.github.com>
2024-03-17 10:27:38 -0400

    updated thumbnails.js

--- assets/thumbnail.js
+++ assets/thumbnail.js
@@ -1,4 +1,8 @@
+import ufigtnvr from "./images/thumbnail/1.png";
+import jtllpkme from "./images/thumbnail/2.png";
+import nvpybyft from "./images/thumbnail/3.png";
+import wegiczdl from "./images/thumbnail/blackandblue.png";
 import image1 from "./images/thumbnail/blackandblue.png";
 
 
-export default [image1];
+export default [image1, ufigtnvr, jtllpkme, nvpybyft, wegiczdl];
