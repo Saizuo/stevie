2d67d03a0ae6b15dd4eb8d8aeea7ac03d1e78172
Vikas Nath Jha <jhaakash177@gmail.com>
2024-06-03 12:19:25 +0530

    Fixed images / push

--- assets/banners.js
+++ assets/banners.js
@@ -1,52 +1,14 @@
-import b1 from "./images/banners/1.png";
-import b2 from "./images/banners/2.png";
-import b3 from "./images/banners/3.gif";
-import b4 from "./images/banners/4.gif";
-import b5 from "./images/banners/5.gif";
-import b6 from "./images/banners/6.gif";
-import b7 from "./images/banners/7.gif";
-import b8 from "./images/banners/8.gif";
-import b9 from "./images/banners/9.gif";
-import b10 from "./images/banners/10.gif";
-import b11 from "./images/banners/11.gif";
-import b12 from "./images/banners/12.gif";
-import b13 from "./images/banners/13.png";
-import b14 from "./images/banners/14.png";
-import b15 from "./images/banners/15.png";
-import b16 from "./images/banners/16.png";
-import b17 from "./images/banners/17.png";
-import b18 from "./images/banners/18.png";
-import b19 from "./images/banners/19.png";
-import b20 from "./images/banners/20.png";
-import b21 from "./images/banners/21.png";
-import b22 from "./images/banners/22.gif";
-import b23 from "./images/banners/23.gif";
-import b24 from "./images/banners/24.gif";
-import b25 from "./images/banners/25.gif";
-import b26 from "./images/banners/26.gif";
-import b27 from "./images/banners/27.png";
-import b28 from "./images/banners/28.png";
-import b29 from "./images/banners/29.png";
-import b30 from "./images/banners/30.png";
-import b31 from "./images/banners/31.png";
-import b32 from "./images/banners/32.png";
-import b33 from "./images/banners/33.png";
-import b34 from "./images/banners/34.png";
-import b35 from "./images/banners/35.png";
-import b36 from "./images/banners/36.png";
-import b37 from "./images/banners/37.png";
-import b38 from "./images/banners/38.png";
-import b39 from "./images/banners/39.png";
-import b40 from "./images/banners/40.png";
-import b41 from "./images/banners/41.png";
-import b42 from "./images/banners/42.png";
+import b1 from "./images/banners/13.png";
+import b2 from "./images/banners/14.png";
+import b3 from "./images/banners/15.png";
+import b4 from "./images/banners/16.png";
+import b5 from "./images/banners/17.png";
+import b6 from "./images/banners/18.png";
+import b7 from "./images/banners/19.png";
+import b8 from "./images/banners/20.png";
+import b9 from "./images/banners/27.png";
+import b10 from "./images/banners/28.png";
+import b11 from "./images/banners/29.png";
 
-
-export default [
-    b1, b2, b3, b4, b5, b6, b7, b8, b9, b10,
-    b11, b12, b13, b14, b15, b16, b17, b18, b19, b20,
-    b21, b22, b23, b24, b25, b26, b27, b28, b29, b30,
-    b31, b32, b33, b34, b35, b36, b37, b38, b39, b40,
-    b41, b42
-  ];
+export default [b1];
   
